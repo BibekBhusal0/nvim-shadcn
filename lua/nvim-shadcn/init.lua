4d3ddb965fb450e6fe7eb3959c9ed2f49899a915
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-03-22 14:43:05 +0545

    Direct instillation from user args

--- lua/nvim-shadcn/init.lua
+++ lua/nvim-shadcn/init.lua
@@ -1,6 +1,7 @@
 local config_module = require('nvim-shadcn.config')
 local config = config_module.config
 local pick_component = require('nvim-shadcn.telescope').pick_component
+local add_component = require('nvim-shadcn.utils').add_component
 
 M = { pick_component = pick_component }
 
@@ -8,7 +9,16 @@ M.setup = function(opts)
   config = vim.tbl_deep_extend('force', config, opts)
   config_module.config = config
 
-  vim.api.nvim_create_user_command('ShadcnAdd', pick_component, {})
+  vim.api.nvim_create_user_command('ShadcnAdd', function(args)
+    local user_args = args.args
+    for _, component in ipairs(config.components) do
+      if component == user_args then
+        add_component(user_args)
+        return
+      end
+    end
+    pick_component()
+  end, { nargs = '*' })
 end
 
 return M
--- lua/nvim-shadcn/telescope.lua
+++ lua/nvim-shadcn/telescope.lua
@@ -1,63 +1,5 @@
-M = {}
-
-local function add_component(component, installer)
-  local config = require('nvim-shadcn.config').config
-  installer = installer or config.default_installer
-  local command_format = config.format[installer]
-  if not command_format then
-    return
-  end
-  local cmd = string.format('echo "yes" | %s', command_format:format(component))
-  local result = vim.fn.system(cmd)
-  if vim.v.shell_error == 0 then
-    print('Component added successfully!')
-  else
-    print('Error adding component: ' .. result)
-  end
-end
-
--- https://github.com/lalitmee/browse.nvim/blob/main/lua/browse/utils.lua
-local get_os_name = function()
-  local os = vim.loop.os_uname()
-  local os_name = os.sysname
-  return os_name
-end
-
--- WSL
-local is_wsl = function()
-  local output = vim.fn.systemlist('uname -r')
-  return not not string.find(output[1] or '', 'WSL')
-end
-
--- get open cmd
-local get_open_cmd = function()
-  local os_name = get_os_name()
-  local open_cmd = nil
-  if os_name == 'Windows_NT' or os_name == 'Windows' then
-    open_cmd = { 'cmd', '/c', 'start' }
-  elseif os_name == 'Darwin' then
-    open_cmd = { 'open' }
-  else
-    if is_wsl() then
-      open_cmd = { 'wsl-open' }
-    else
-      open_cmd = { 'xdg-open' }
-    end
-  end
-  return open_cmd
-end
-
--- start the browser job
-local open_browser = function(target)
-  target = vim.fn.trim(target)
-  local open_cmd = vim.fn.extend(get_open_cmd(), { target })
-  vim.fn.jobstart(open_cmd, { detach = true })
-end
-
-local open_doc = function(component)
-  local config = require('nvim-shadcn.config').config
-  open_browser(string.format(config.format.doc:format(component)))
-end
+local add_component = require('nvim-shadcn.utils').add_component
+local open_doc = require('nvim-shadcn.utils').open_doc
 
 local function pick_component()
   local config = require('nvim-shadcn.config').config
@@ -91,7 +33,6 @@ local function pick_component()
               end)
             )
           end
-          -- map(key, value.doc, run_action(add_component))
         end
         for key, value in pairs(config.keys.n) do
           if key == 'doc' then
@@ -105,9 +46,7 @@ local function pick_component()
               end)
             )
           end
-          -- map(key, value.doc, run_action(add_component))
         end
-        -- map({ 'i', 'n' }, '<C-o>', run_action(open_doc))
         return true
       end,
     })
