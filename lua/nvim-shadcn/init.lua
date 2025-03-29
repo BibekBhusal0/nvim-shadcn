3dd72d0cd9ed494d82f5201500c26ce1860e9905
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-03-29 13:49:08 +0545

    ✨ feat: allow multiple components to be added at once

--- lua/nvim-shadcn/init.lua
+++ lua/nvim-shadcn/init.lua
@@ -11,13 +11,41 @@ M.setup = function(opts)
 
   vim.api.nvim_create_user_command('ShadcnAdd', function(args)
     local user_args = args.args
-    for _, component in ipairs(config.components) do
-      if component == user_args then
-        add_component(user_args)
-        return
+
+    -- Split the arguments by spaces
+    if user_args and string.find(user_args, '%s') then
+      local components = vim.split(user_args, '%s')
+      for _, component in ipairs(components) do
+        local found = false
+        for _, valid_component in ipairs(config.components) do
+          if valid_component == component then
+            add_component(component)
+            found = true
+            break
+          end
+        end
+        if not found then
+          print(string.format("Component '%s' not found in config.components", component))
+        end
       end
+
+      -- Single argument case
+    elseif user_args then
+      local found = false
+      for _, component in ipairs(config.components) do
+        if component == user_args then
+          add_component(user_args)
+          found = true
+          break
+        end
+      end
+      if not found then
+        pick_component()
+      end
+    else
+      -- No arguments provided
+      pick_component()
     end
-    pick_component()
   end, { nargs = '*' })
 end
 
