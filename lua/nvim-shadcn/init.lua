51256d52f8cf539f2e1f30e2a559f2c5572e9019
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-14 20:33:28 +0545

    chore: 🔧 move the user commands to plugins for auto complete

--- lua/nvim-shadcn/init.lua
+++ lua/nvim-shadcn/init.lua
@@ -1,7 +1,6 @@
 local config_module = require('nvim-shadcn.config')
 local config = config_module.config
 local pick_component = require('nvim-shadcn.telescope').pick_component
-local add_component = require('nvim-shadcn.utils').add_component
 local init = require('nvim-shadcn.utils').init
 
 M = { pick_component = pick_component, init = init }
@@ -9,55 +8,6 @@ M = { pick_component = pick_component, init = init }
 M.setup = function(opts)
   config = vim.tbl_deep_extend('force', config, opts)
   config_module.config = config
-
-  vim.api.nvim_create_user_command('ShadcnInit', function()
-    init()
-  end, {})
-
-  vim.api.nvim_create_user_command('ShadcnAddImportant', function()
-    for _, component in ipairs(config.important) do
-      add_component(component)
-    end
-  end, {})
-
-  vim.api.nvim_create_user_command('ShadcnAdd', function(args)
-    local user_args = args.args
-
-    -- Split the arguments by spaces
-    if user_args and string.find(user_args, '%s') then
-      local components = vim.split(user_args, '%s')
-      for _, component in ipairs(components) do
-        local found = false
-        for _, valid_component in ipairs(config.components) do
-          if valid_component == component then
-            add_component(component)
-            found = true
-            break
-          end
-        end
-        if not found then
-          print(string.format("Component '%s' not found in config.components", component))
-        end
-      end
-
-      -- Single argument case
-    elseif user_args then
-      local found = false
-      for _, component in ipairs(config.components) do
-        if component == user_args then
-          add_component(user_args)
-          found = true
-          break
-        end
-      end
-      if not found then
-        pick_component()
-      end
-    else
-      -- No arguments provided
-      pick_component()
-    end
-  end, { nargs = '*' })
 end
 
 return M
---
+++ plugin/nvim-shadcn.lua
@@ -0,0 +1,54 @@
+vim.api.nvim_create_user_command('ShadcnInit', function()
+  require('nvim-shadcn.utils').init()
+end, {})
+
+vim.api.nvim_create_user_command('ShadcnAddImportant', function()
+  local config_module = require('nvim-shadcn.config')
+  local config = config_module.config
+  for _, component in ipairs(config.important) do
+    require('nvim-shadcn.utils').add_component(component)
+  end
+end, {})
+
+vim.api.nvim_create_user_command('ShadcnAdd', function(args)
+  local config_module = require('nvim-shadcn.config')
+  local config = config_module.config
+  local add_component = require('nvim-shadcn.utils').add_component
+  local user_args = args.args
+  local pick_component = require('nvim-shadcn.telescope').pick_component
+
+  -- Split the arguments by spaces
+  if user_args and string.find(user_args, '%s') then
+    local components = vim.split(user_args, '%s')
+    for _, component in ipairs(components) do
+      local found = false
+      for _, valid_component in ipairs(config.components) do
+        if valid_component == component then
+          add_component(component)
+          found = true
+          break
+        end
+      end
+      if not found then
+        print(string.format("Component '%s' not found in config.components", component))
+      end
+    end
+
+    -- Single argument case
+  elseif user_args then
+    local found = false
+    for _, component in ipairs(config.components) do
+      if component == user_args then
+        add_component(user_args)
+        found = true
+        break
+      end
+    end
+    if not found then
+      pick_component()
+    end
+  else
+    -- No arguments provided
+    pick_component()
+  end
+end, { nargs = '*' })
